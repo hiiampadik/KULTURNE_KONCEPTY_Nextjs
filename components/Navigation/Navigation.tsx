9531708acc5cb20738d838b08eb66de1f86ca2d6
Bronislav Musil <brona.musil@gmail.com>
2026-04-11 13:09:15 +0200

    Overlay

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
-import React, {FunctionComponent} from 'react'
+'use client'
+import React, {FunctionComponent, useState} from 'react'
 import {useTranslations} from 'next-intl'
 import {LocaleSwitcher} from './LocaleSwitcher'
 import {ThemeToggle} from './ThemeToggle'
@@ -6,44 +7,55 @@ import {socialLinks} from '@/constants/site'
 import styles from './Navigation.module.scss'
 import {classNames} from '@/components/utils/classNames';
 import {DogEar} from '@/components/DogEar/DogEar';
+import {OverlayContact} from '@/components/OverlayContact/OverlayContact';
 
 export const Navigation: FunctionComponent = () => {
     const t = useTranslations('Navigation')
+    const [contactOpen, setContactOpen] = useState(false)
 
     return (
-        <nav className={styles.nav}>
+        <>
+            <nav className={styles.nav}>
 
-            <DogEar corner={'top-right'} shadow={true}>
-                <div className={styles.top}>
-                    <LocaleSwitcher/>
-                    <a href="#" className={styles.contact}>{t('contact')}</a>
-                    <div className={styles.social}>
-                        <a href={socialLinks.instagram} className={styles.socialLink} target="_blank"
-                           rel="noopener noreferrer">IG</a>
-                        <a href={socialLinks.facebook} className={styles.socialLink} target="_blank"
-                           rel="noopener noreferrer">FB</a>
+                <DogEar corner={'top-right'} shadow={true}>
+                    <div className={styles.top}>
+                        <LocaleSwitcher/>
+                        <button
+                            className={styles.contact}
+                            onClick={() => setContactOpen(prev => !prev)}
+                        >
+                            {t('contact')}
+                        </button>
+                        <div className={styles.social}>
+                            <a href={socialLinks.instagram} className={styles.socialLink} target="_blank"
+                               rel="noopener noreferrer">IG</a>
+                            <a href={socialLinks.facebook} className={styles.socialLink} target="_blank"
+                               rel="noopener noreferrer">FB</a>
+                        </div>
+                    </div>
+                </DogEar>
+
+                <div className={styles.bottom}>
+                    <div className={styles.logo}>
+                        <img src="/KK_LOGO.svg" alt="Kultúrne Koncepty" className={styles.logoImage}/>
+                    </div>
+
+                    <div className={styles.menu}>
+                        <DogEar corner={'bottom-right'} shadow={true}>
+                            <a className={classNames([styles.menuItem, styles.item1])} href="#who-we-are" onClick={() => setContactOpen(false)}>{t('menu.whoWeAre')}</a>
+                        </DogEar>
+                        <a className={classNames([styles.menuItem, styles.item2])} href="#fields" onClick={() => setContactOpen(false)}>{t('menu.fields')}</a>
+                        <DogEar corner={'bottom-right'} shadow={true}>
+                            <a className={classNames([styles.menuItem, styles.item3])} href="#projects" onClick={() => setContactOpen(false)}>{t('menu.projects')}</a>
+                        </DogEar>
                     </div>
                 </div>
-            </DogEar>
-
-            <div className={styles.bottom}>
-                <div className={styles.logo}>
-                    <img src="/KK_LOGO.svg" alt="Kultúrne Koncepty" className={styles.logoImage}/>
-                </div>
-
-                <div className={styles.menu}>
-                    <DogEar corner={'bottom-right'} shadow={true}>
-                        <a className={classNames([styles.menuItem, styles.item1])} href="#who-we-are">{t('menu.whoWeAre')}</a>
-                    </DogEar>
-                    <a className={classNames([styles.menuItem, styles.item2])} href="#fields">{t('menu.fields')}</a>
-                    <DogEar corner={'bottom-right'} shadow={true}>
-                        <a className={classNames([styles.menuItem, styles.item3])} href="#projects">{t('menu.projects')}</a>
-                    </DogEar>
-                </div>
-            </div>
 
 
-            <ThemeToggle/>
-        </nav>
+                <ThemeToggle/>
+            </nav>
+
+            <OverlayContact isOpen={contactOpen} handleClose={() => setContactOpen(false)}/>
+        </>
     )
 }
--- components/Overlay/index.tsx
+++ components/Overlay/index.tsx
@@ -1,38 +1,61 @@
 'use client'
-import {FunctionComponent, PropsWithChildren} from 'react'
-import {Overlay2} from '@blueprintjs/core'
-import {useTranslations} from 'next-intl'
+import {FunctionComponent, PropsWithChildren, useEffect} from 'react'
 import styles from './index.module.scss'
 import {classNames} from '@/components/utils/classNames'
 
+export type OverlayIcon = 'architecture' | 'culture' | 'urbanism' | 'design' // TODO: přiřadit SVG ikonám
+
 interface OverlayProps {
-    readonly isOpen?: boolean
+    readonly isOpen: boolean
     readonly handleClose: () => void
     readonly className?: string
-    readonly scrollable?: boolean
+    readonly icons?: OverlayIcon[]
 }
 
-const Overlay: FunctionComponent<PropsWithChildren<OverlayProps>> = ({isOpen = true, scrollable = true, handleClose, children, className}) => {
-    const t = useTranslations('Overlay')
+const Overlay: FunctionComponent<PropsWithChildren<OverlayProps>> = ({isOpen, handleClose, children, className, icons}) => {
+    useEffect(() => {
+        const html = document.documentElement
+        const body = document.body
+        if (isOpen) {
+            html.style.overflow = 'hidden'
+            body.style.overflow = 'hidden'
+        } else {
+            html.style.overflow = ''
+            body.style.overflow = ''
+        }
+        return () => {
+            html.style.overflow = ''
+            body.style.overflow = ''
+        }
+    }, [isOpen])
 
     return (
         <>
-            {isOpen &&
-                <Overlay2
-                    isOpen={true}
-                    onClose={() => handleClose()}
-                    hasBackdrop={true}
-                >
-                    <div className={classNames([styles.container, scrollable ? styles.scrollable : styles.notScrollable])}>
-                        <button className={styles.closeButton} onClick={() => handleClose()}>
-                            {t('close')}
-                        </button>
-                        <div className={className}>
-                            {children}
+            <div
+                className={classNames([styles.backdrop, isOpen && styles.open])}
+                onClick={handleClose}
+            />
+            <div className={classNames([styles.panel, isOpen && styles.open, className])}>
+                <div className={styles.toolbar}>
+                    <button className={styles.closeButton} onClick={handleClose} aria-label="Close">
+                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="2" y1="2" x2="28" y2="28" stroke="currentColor" strokeWidth="1.5"/>
+                            <line x1="28" y1="2" x2="2" y2="28" stroke="currentColor" strokeWidth="1.5"/>
+                        </svg>
+                    </button>
+                    {icons && icons.length > 0 && (
+                        <div className={styles.icons}>
+                            {/* TODO: nahradit skutečnými SVG ikonami */}
+                            {icons.map(icon => (
+                                <span key={icon} className={styles.icon} data-icon={icon}/>
+                            ))}
                         </div>
-                    </div>
-                </Overlay2>
-            }
+                    )}
+                </div>
+                <div className={classNames([styles.content, className])}>
+                    {children}
+                </div>
+            </div>
         </>
     )
 }
