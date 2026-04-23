4b46fffa8df65b8f86fc6c48f7b89fd20c292f36
Bronislav Musil <brona.musil@gmail.com>
2026-04-23 12:34:15 +0200

    Trailing slash

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true'
 const nextConfig: NextConfig = {
     ...(isGithubPages && {
         output: 'export',
+        trailingSlash: true,
         images: {
             unoptimized: true,
         },
